4eb4095a8338fe308cd12a4df661ac111e639883
Tokisaki-Kurumi-X <2771319167@qq.com>
2025-06-21 10:19:47 +0800

    新建的模板

---
+++ src/main/java/com/example/unity_backend/UnityBackEndApplication.java
@@ -0,0 +1,11 @@
+package com.example.unity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnityBackEndApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UnityBackEndApplication.class,args);
+    }
+}
